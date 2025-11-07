7cc9aa303e2721fa69b10760aefe2968e60e78c5
felip-SN <felipenascimentooudri@gmail.com>
2025-11-07 00:13:14 -0300

    commit inicial, projeto criado e classe pessoa criada

---
+++ prjPOOFelipeHeber/src/fatec/poo/model/Pessoa.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fatec.poo.model;
+
+/**
+ *
+ * @author felip
+ */
+public class Pessoa {
+    private String cpf;
+    private String nome;
+    private String endereco;
+    private  String telefone;
+
+    public Pessoa(String cpf, String nome) {
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
